c56a4e050ddc28ee40b0a802e2e11a2742ff0181
Bushra1995 <bushra95ga@gmail.com>
2023-04-30 21:10:02 +0300

    build the basic website

---
+++ sign up.js
@@ -0,0 +1,4 @@
+const loginButton = document.getElementById("login");
+loginButton.addEventListener("click", function () {
+    window.location.href = "labPage.html"; // replace "home.html" with the actual URL of your home page
+});
